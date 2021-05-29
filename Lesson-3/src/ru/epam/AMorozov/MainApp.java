646753958e89811ad9849dcf603ffa6324584809
MorozovGeeK <morozov4811@gmail.com>
2021-05-29 19:24:41 +0400

    hw-3

---
+++ Lesson-3/src/ru/epam/AMorozov/MainApp.java
@@ -0,0 +1,11 @@
+package ru.epam.AMorozov;
+
+public class MainApp {
+    public static void main(String[] args) {
+        String firstWord = "Hello";
+        String secondWord = "World";
+        System.out.println(firstWord + ", " + secondWord + "!");
+        System.out.println();
+        System.out.println("Hello, World!");
+    }
+}
